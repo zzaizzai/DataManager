18bb8359ebf080cc8aa0f24867413b7181c1fc4b
zzaizzai <junsai0530@gmail.com>
2022-10-30 17:55:09 +0900

    dd

--- DataManager.py
+++ DataManager.py
@@ -20,33 +20,31 @@ if __name__ == "__main__":
         len_dir = len(str(os.environ.get('GLOB_FILE_PATH'))) + 1
 
         file_list = glob.glob(str(os.environ.get('GLOB_FILE_PATH')) + r'\**\*.xl*', recursive=True)
-        # print(file_list)
+
         df_new = pd.DataFrame()
+
         for file in file_list:
             # making_time = datetime.datetime.strptime(time.ctime(os.path.getctime(file)), "%a %b %d %H:%M:%S %Y")
             making_time = datetime.datetime.strptime(time.ctime(os.path.getctime(file)), "%c")
             modify_time = time.ctime(os.path.getmtime(file))
             file_name = pathlib.Path(file).stem
-            # print(file_name, making_time )
             df_new = df_new.append(pd.DataFrame([[file_name,making_time, modify_time,  file]], columns=[ 'file_name', 'making_time','modify_time', 'file_path']))
 
-        print(df_new)
-        # df_new.to_excel('DB.xlsx', index=False)
 
         df_old = pd.read_excel('./DB.xlsx')
-        print(df_old)
-
+        # print(df_old)
+        print()
         path_list_new = df_new['file_path'].to_list()
         for i, path in enumerate(path_list_new):
             if len(df_old.query(f'file_path == @path')) == 0:
-                print(df_new.query("file_path == @path").loc[:,['making_time']])
                 print('new data: ', path[len_dir:], ' ,Date: ' ,df_new['making_time'].to_list()[i])
 
             elif len(df_old.query(f'file_path == @path')) > 0:
                 if df_old.query(f'file_path == @path')['modify_time'].values[0] != df_new.query(f'file_path == @path')['modify_time'].values[0]:
                     print( 'modifided: ',path[len_dir:], 'Date: ', time_normal(df_old.query(f'file_path == @path')['modify_time'].values[0]), '-->' ,time_normal(df_new.query(f'file_path == @path')['modify_time'].values[0]))
-
+        print()
         process_do = input('Continue(any)? or Save(save): ')
+        
         if process_do == "save":
             df_new.to_excel('DB.xlsx', index=False)
             print('saved new data')
